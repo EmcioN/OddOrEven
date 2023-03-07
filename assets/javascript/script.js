22a04583c65694c4cbc8afde78b94ec283f194cf
EmcioN <villly1990@gmail.com>
2023-03-07 16:55:44 +0000

    Add style to footer, add comments

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,38 +1,36 @@
-const playerNumberInput = document.getElementById('player-number');
-const evenButton = document.getElementById('even-button');
-const oddButton = document.getElementById('odd-button');
-const aiNumberSpan = document.getElementById('ai-number');
-const resultSpan = document.getElementById('result');
+// Generate a random number 
 
-// Generate a random number between 1 and 10 for the AI
 function generateAINumber() {
     return Math.floor(Math.random() * 10) + 1;
 }
 
 // Check if a number is even
+
 function isEven(number) {
     return number % 2 === 0;
 }
 
-// Listen for clicks on the even and odd buttons
-evenButton.addEventListener('click', function() {
-    const playerNumber = parseInt(playerNumberInput.value);
+// Event listeners for both buttons
+
+const evenButton = document.getElementById('even-button');
+const oddButton = document.getElementById('odd-button');
+const aiNumberSpan = document.getElementById('ai-number');
+const resultSpan = document.getElementById('result');
+
+evenButton.addEventListener('click', function() {    
     const aiNumber = generateAINumber();
-    aiNumberSpan.textContent = aiNumber;
-    const total = playerNumber + aiNumber;
-    if (isEven(total)) {
+    aiNumberSpan.textContent = aiNumber;    
+    if (isEven(aiNumber)) {
       resultSpan.textContent = 'You win!';
     } else {
       resultSpan.textContent = 'You lose!';
     }
 });
 
-oddButton.addEventListener('click', function() {
-    const playerNumber = parseInt(playerNumberInput.value);
+oddButton.addEventListener('click', function() {    
     const aiNumber = generateAINumber();
-    aiNumberSpan.textContent = aiNumber;
-    const total = playerNumber + aiNumber;
-    if (!isEven(total)) {
+    aiNumberSpan.textContent = aiNumber;    
+    if (!isEven(aiNumber)) {
       resultSpan.textContent = 'You win!';
     } else {
       resultSpan.textContent = 'You lose!';
